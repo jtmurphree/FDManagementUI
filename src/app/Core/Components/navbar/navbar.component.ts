99804fc6987264322fb4c227146c88032b09fd94
Jason Murphree <jtmurphree@gmail.com>
2024-03-21 00:22:49 -0500

    made updates to navbar and pulling user data for the users list.

--- src/app/Core/Components/navbar/navbar.component.ts
+++ src/app/Core/Components/navbar/navbar.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { routes } from '../../../app.routes';
+import { NgModule } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from '../../../Features/Home/home/home.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterModule, RouterOutlet, HomeComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- src/app/Features/Users/users-list/users-list.component.ts
+++ src/app/Features/Users/users-list/users-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/user.service';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit{
+  users$?: Observable<User[]>;
 
+  constructor(private userService: UserService){
+
+  }
+
+  ngOnInit(): void {
+    this.users$ = this.userService.getAllusers();
+  }
 }
+
