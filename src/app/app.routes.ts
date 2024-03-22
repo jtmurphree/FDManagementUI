db8bcd7d2531e3c8bc06c118f52c84de48da82db
Jason Murphree <jtmurphree@gmail.com>
2024-03-21 19:36:47 -0500

    updated name of apparatus list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './Features/Users/users-list/users-list.component';
 import { HomeComponent } from './Features/Home/home/home.component';
-import { ApparatusComponent } from './Features/Apparatus/apparatus/apparatus.component';
+import { ApparatusListComponent } from './Features/Apparatus/apparatus-list/apparatus-list.component';
 
 export const routes: Routes = [
     {
@@ -10,7 +10,7 @@ export const routes: Routes = [
     },
     {
         path: 'admin/apparatus',
-        component: ApparatusComponent
+        component: ApparatusListComponent
     },
     {
         path: '',
