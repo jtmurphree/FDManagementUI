e074db02ec05130fa00b3684b8a514933b693d2a
Jason Murphree <jtmurphree@gmail.com>
2024-03-21 23:57:38 -0500

    minor corrections

--- src/app/Core/Components/navbar/navbar.component.ts
+++ src/app/Core/Components/navbar/navbar.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { routes } from '../../../app.routes';
 import { NgModule } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { HomeComponent } from '../../../Features/Home/home/home.component';
+;
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, HomeComponent],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
