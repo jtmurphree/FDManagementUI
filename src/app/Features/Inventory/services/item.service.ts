3c24d3ea70b73498e5aca49f2b22064b16aeb256
Jason Murphree <jtmurphree@gmail.com>
2024-03-21 23:27:03 -0500

    added items component

--- src/app/Features/Inventory/services/item.service.ts
+++ src/app/Features/Inventory/services/item.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment.development';
+import { Item } from '../models/item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Item[]>{
+    return this.http.get<Item[]>(`${environment.apiBaseUrl}/api/Items`);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './Features/Users/users-list/users-list.component';
 import { HomeComponent } from './Features/Home/home/home.component';
+import { ItemListComponent } from './Features/Inventory/item-list/item-list.component';
 
 export const routes: Routes = [
     {
         path: 'admin/users',
         component: UsersListComponent
+    },
+    {
+        path: 'admin/items',
+        component: ItemListComponent
     },
     {
         path: '',
