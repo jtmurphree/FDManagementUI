0d025847b28896514b4a97a34f88af5d6e601164
Jason Murphree <jtmurphree@gmail.com>
2024-03-25 23:27:44 -0500

    added controls to select drive, fuel, and type for apparatus

--- src/app/Features/Apparatus/Services/apparatus.service.ts
+++ src/app/Features/Apparatus/Services/apparatus.service.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment.development';
 import { AddApparatusRequest } from '../models/addApparatusRequest.model';
+import { ApparatusType } from '../models/apparatusType.model';
+import { FuelType } from '../models/fuelType.model';
+import { DriveType } from '../models/driveType.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +21,17 @@ export class ApparatusService {
 
   addApparatus(model: AddApparatusRequest): Observable<void>{
     return this.http.post<void>(`${environment.apiBaseUrl}/api/Apparatus`, model);
+  }
+
+  getApparatusTypes(): Observable<ApparatusType[]>{
+    return this.http.get<ApparatusType[]>(`${environment.apiBaseUrl}/api/Apparatus/types`);
+  }
+
+  getFuelTypes(): Observable<FuelType[]>{
+    return this.http.get<FuelType[]>(`${environment.apiBaseUrl}/api/Apparatus/fueltypes`);
+  }
+
+  getDriveTypes(): Observable<DriveType[]>{
+    return this.http.get<FuelType[]>(`${environment.apiBaseUrl}/api/Apparatus/drivetypes`);
   }
 }
--- src/app/Features/Apparatus/add-apparatus/add-apparatus.component.ts
+++ src/app/Features/Apparatus/add-apparatus/add-apparatus.component.ts
@@ -5,33 +5,97 @@ import { Subscription } from 'rxjs';
 import { ApparatusService } from '../Services/apparatus.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { ApparatusType } from '../models/apparatusType.model';
+import { DriveType } from '../models/driveType.model';
+import { FuelType } from '../models/fuelType.model';
 
 @Component({
   selector: 'app-add-apparatus',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, NgFor],
   templateUrl: './add-apparatus.component.html',
   styleUrl: './add-apparatus.component.css'
 })
 
 export class AddApparatusComponent {
   model: AddApparatusRequest;
-  private addApparatusSubscription?: Subscription
+  private addApparatusSubscription?: Subscription;
+  private apparatusTypeSubscription?: Subscription;
+  private fuelTypeSubscription?: Subscription;
+  private driveTypeSubscription?: Subscription;
+
+  private apparatusType: string = '';
+  private fueltype: string = '';
+  private driveType: string = '';
+
+  apparatusTypes: ApparatusType[] = [];
+  fuelTypes: FuelType[] = [];
+  driveTypes: DriveType[] = [];
 
   constructor(private appService: ApparatusService, private router: Router){
     this.model = new AddApparatusRequest;
+    this.getApparatusTypes();
+    this.getFuelTypes();
+    this.getDriveTypes();
+    this.apparatusType = '1';
+    this.fueltype = '1';
+    this.driveType = '1';
   }
 
   ngOnDestroy(): void {
     this.addApparatusSubscription?.unsubscribe();
+    this.apparatusTypeSubscription?.unsubscribe();
+    this.fuelTypeSubscription?.unsubscribe();
+    this.driveTypeSubscription?.unsubscribe();
   }
 
   OnFormSubmit(): void {
+    this.model.apparatusTypeId = +this.apparatusType;
     this.addApparatusSubscription = this.appService.addApparatus(this.model)
     .subscribe({
       next: (response) => {
         this.router.navigateByUrl('admin/apparatus');
       }
     });
+  }
+
+  onTypeSelection(event: Event):void{
+    this.apparatusType = (event.target as HTMLSelectElement).value;
+  }
+
+  onFuelSelection(event: Event): void {
+    this.fueltype = (event.target as HTMLSelectElement).value;
+  }
+
+  onDriveSelection(event: Event): void {
+    this.driveType = (event.target as HTMLSelectElement).value;
+  }
+
+  getApparatusTypes(): void {
+    this.apparatusTypeSubscription = this.appService.getApparatusTypes()
+    .subscribe({
+      next: (response) => {
+        this.apparatusTypes = response;
+      }          
+    });
+  }
+
+  getFuelTypes(): void {
+    this.fuelTypeSubscription = this.appService.getFuelTypes()
+    .subscribe({
+      next: (response) => {
+        this.fuelTypes = response;
+      }
+    })
+  }
+
+  getDriveTypes(): void {
+    this.driveTypeSubscription = this.appService.getDriveTypes()
+    .subscribe({
+      next: (response) => {
+        this.driveTypes = response;
+      }
+    })
   }
 }
