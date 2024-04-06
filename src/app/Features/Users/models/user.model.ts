fea1b1d0fac1ced48e05e0aa4b0391ef0c54dfbf
Jason Murphree <jtmurphree@gmail.com>
2024-04-05 22:46:51 -0500

    working on user detail

--- src/app/Features/Users/models/user.model.ts
+++ src/app/Features/Users/models/user.model.ts
@@ -1,11 +1,25 @@
-export interface User{
+export class User{
     id: number;
     firstName: string;
     lastName: string;
+    userName: string;
     displayName: string;
     employeeId: string;
     phoneNumber: string;
     email: string;
     userRoleName: string;
     dateAdded: Date;
+
+    constructor(){
+        this.id = 0
+        this.firstName = '';
+        this.lastName = '';
+        this.userName  = '';
+        this.displayName = '';
+        this.employeeId = '';
+        this.phoneNumber = '';
+        this.email= '';
+        this.userRoleName = ''
+        this.dateAdded = new Date();
+    }
 }
--- src/app/Features/Users/user-details/user-details.component.ts
+++ src/app/Features/Users/user-details/user-details.component.ts
@@ -3,27 +3,31 @@ import { UserService } from '../Services/user.service';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterLink, RouterLinkActive, MatButton, MatFormFieldModule,
-    MatInputModule, MatSelectModule],
+    MatInputModule, MatSelectModule, MatCardModule, MatDividerModule, ReactiveFormsModule],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css'
 })
 export class UserDetailsComponent implements OnInit, OnDestroy{
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   user$?: Observable<User>;
   userId: number;
+  model: User;
   constructor(private userService: UserService){
     this.userId = 1;
-
+    this.model = new User();
   }
 
   ngOnInit(): void {
@@ -32,5 +36,9 @@ export class UserDetailsComponent implements OnInit, OnDestroy{
 
   ngOnDestroy(): void {
     
+  }
+
+  OnFormSubmit(): void{
+
   }
 }
